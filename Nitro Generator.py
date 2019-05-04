def7bcbeae4c4763c88f6e7721bdb7542ef92a66
BatuHansn <32800002+BatuHansn@users.noreply.github.com>
2019-05-04 12:31:18 +0300

    Add files via upload

---
+++ Nitro Generator.py
@@ -0,0 +1,15 @@
+import random
+import string
+from lxml.html import fromstring
+
+print("\nCoded By DiscordShit on Nulled.to!\n")
+
+times = int(input("How many codes do you want to generate?: "))
+
+file = open("codes.txt","w")
+
+for i in range(times):
+	codes = ''.join(random.sample((string.ascii_lowercase+string.ascii_uppercase+string.digits),16))
+	file.write(codes)
+	file.write("\n")
+print("\nFinished! Make sure to like my thread :) - If you have issues with the program writing to the codes.txt file, you need to cd to the directory this python file is in. Example: C:/Users/Hackerman/Desktop/NitroGenerator/")
